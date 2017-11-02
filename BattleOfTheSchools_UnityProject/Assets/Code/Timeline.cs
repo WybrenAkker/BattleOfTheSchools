03678b069ac8fd69f7a8949d8a43a93cb7358f2b
WybrenAkker <wybren.akker@gmail.com>
2017-11-02 15:48:21 +0100

    TimeLine Implemented -Needs:        -TimeLine(int posOnTimeLine) in Graph        -UpdateSim(int timeSnippet, int simTime) in MapConverter

---
+++ BattleOfTheSchools_UnityProject/Assets/Code/Timeline.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Timeline : MonoBehaviour
+{
+    public float maxSnapDif;
+    public Slider slider;
+    GameObject gameManager;
+    MapConverter mc;
+    int imaginarySecondsBetweenHours;
+    public GameObject graph;
+
+    public void Start()
+    {
+        gameManager = GameObject.Find("GameManager");
+        mc = gameManager.GetComponent<MapConverter>();
+        //imaginarySecondsBetweenHours = mc.imaginaryTimeBetweenHours;
+    }
+
+    public void OnValueChanged()
+    {
+        int nearestValue = Mathf.RoundToInt(slider.value);
+
+        float dif = nearestValue - slider.value;
+
+        if(dif < maxSnapDif && dif > 0)
+        {
+            slider.value = nearestValue;
+        }
+        else if(dif < 0 && dif > maxSnapDif)
+        {
+            slider.value = nearestValue;
+        }
+
+        int timeSnippet = Mathf.FloorToInt(slider.value);
+
+        float timeDif = slider.value - timeSnippet;
+
+        int simTime = Mathf.RoundToInt(imaginarySecondsBetweenHours * timeDif);
+
+        //mc.UpdateSim(timeSnippet, simTime);
+        //graph.GetComponent<Graph>().TimeLine(slider.value);
+    }
+}
